e0a75057a4b5a5e86b6ea1c3f054f8ea7fe855e6
Jeremi Concepcion <jeremi.concepcion@gmail.com>
2014-11-23 16:33:35 -0600

    Tried changing Twitter auth keys in twitter.rb and grackletweet2.rb

--- db/grackletweet2.rb
+++ db/grackletweet2.rb
@@ -5,10 +5,10 @@ require 'highline/import'
 
 $client = Grackle::Client.new(:outh => {
 	:type => :oauth,
-	:consumer_key => 'tJO12Hz2jQGHYEZZNWuEg',
-	:consumer_secret => 'u6izuQhp6e27MYAF8HeNMttZIClDlynD15nEUwrHSE',
-	:token => '868988989-SM0X85NmAzJPhVPLKUEj9SS2Ab3mBIbW7qLFpQIE',
-	:token_secret => 'rVRqjYtBSLWEtX1aZrqa9P21CAMk92GfUsCC1wL8PY7Ht'
+	:consumer_key = 'ChH4jjRPwOn4l9ptn9q6ZZ2De',
+	:consumer_secret = '1AgZCRJBtRQiXrCJNEjfRZsRC4dqVO6dj54VF4YvMFqT5M2fww',
+	:token = '2837796245-TvfIANDk55Ek0iWSkdSlMiRofFCvOBz9GEqN1Xl',
+	:token_secret = 'ebK4ChVc866ZEqW2IrF4IwpVwAKK7wvzJGHDtan3ZdfHB'
 	})
 	
 class Twitter
